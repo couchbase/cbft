60ec49742f3fc5bd9b084e7ba4002b186a3d2c94
Steve Yen <steve.yen@gmail.com>
2014-10-13 11:12:42 -0700

    added cpuProfile/memProfile flags

--- main.go
+++ main.go
@@ -10,5 +10,44 @@
 //  governing permissions and limitations under the License.
 package main
 
+import (
+	_ "expvar"
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	"github.com/blevesearch/bleve"
+	// bleveHttp "github.com/blevesearch/bleve/http"
+)
+
+var indexPath = flag.String("index", "index.cbft", "path to index file")
+var cpuProfile = flag.String("cpuProfile", "", "write cpu profile to file")
+var memProfile = flag.String("memProfile", "", "write mem profile to file")
+
 func main() {
+	flag.Parse()
+
+	log.Printf("GOMAXPROCS: %d", runtime.GOMAXPROCS(-1))
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		// TODO: Need to close f somewhere?
+	}
+
+	_, err := bleve.Open(*indexPath)
+	if err == bleve.ErrorIndexPathDoesNotExist {
+		log.Printf("creating new index: %s", *indexPath)
+		log.Fatal("TODO: implement creating new index")
+	} else if err != nil {
+		log.Fatalf("error: could not open index: %s, err: %v", *indexPath, err)
+	}
+
+	// TODO: Maintain the index.
+	// TODO: Spin up REST.
 }
