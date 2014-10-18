998d917be1cbd74934d873b3f6b40625b0df3423
Steve Yen <steve.yen@gmail.com>
2014-10-17 17:36:21 -0700

    more skeleton of CalcPlan()

--- manager_planner.go
+++ manager_planner.go
@@ -11,14 +11,49 @@
 
 package main
 
+import (
+	"fmt"
+
+	log "github.com/couchbaselabs/clog"
+)
+
+const PLANNER_VERSION = "0.0.0" // Follow semver rules.
+
+type Plan struct {
+}
+
+type LogicalIndex struct {
+}
+
+type LogicalIndexes []*LogicalIndex
+
+type Indexer struct {
+}
+
+type Indexers []*Indexer
+
 // A planner assigns partitions to cbft's and to PIndexes on each cbft.
 func (mgr *Manager) PlannerLoop() {
 	for _ = range mgr.plannerCh {
-		// TODO: implement the grand plans for the planner.
-		// First gen planner should keep it simple, such as...
-		// - a single Feed for every datasource node.
-		// - a Feed might "fan out" to multiple Streams/PIndexes.
-		// - have a single PIndex for all datasource partitions
-		//   (vbuckets) to start.
+		currAllowedPlannerVersion := "0.0"
+		plan, err := mgr.CalcPlan(nil, nil, currAllowedPlannerVersion)
+		if err != nil {
+			log.Printf("error: CalcPlan, err: %v", err)
+		}
+		if plan != nil {
+			// TODO: save the plan.
+		}
 	}
+}
+
+func (mgr *Manager) CalcPlan(logicalIndexes LogicalIndexes,
+	indexers Indexers,
+	allowPlannerVersion string) (*Plan, error) {
+	// TODO: implement the grand plans for the planner.
+	// First gen planner should keep it simple, such as...
+	// - a single Feed for every datasource node.
+	// - a Feed might "fan out" to multiple Streams/PIndexes.
+	// - have a single PIndex for all datasource partitions
+	//   (vbuckets) to start.
+	return nil, fmt.Errorf("TODO")
 }
