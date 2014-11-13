2a05c399d4c5ee0cdc99e8b76176e072456f5a3f
Steve Yen <steve.yen@gmail.com>
2014-11-13 09:16:00 -0800

    comments on Cfg interface

--- cfg.go
+++ cfg.go
@@ -11,27 +11,32 @@
 
 package main
 
+// Cfg is the interface that configuration providers must implement.
 type Cfg interface {
-	// A zero cas means don't do a CAS match on Get().
+	// A zero cas means don't do a CAS match on Get(), and a non-zero
+	// cas value means the Get() will only succeed if the CAS matches.
 	Get(key string, cas uint64) (val []byte, casSuccess uint64, err error)
 
-	// A zero cas means must be a create.
+	// A zero cas means the Set() operation must be an entry creation.
+	// So, a zero cas Set() will error if the entry already exists.  A
+	// non-zero, non-matching cas value will error.
 	Set(key string, val []byte, cas uint64) (casSuccess uint64, err error)
 
-	// A zero cas means don't match CAS on Del().
+	// A zero cas means don't match CAS on Del().  Otherwise, a
+	// non-zero, non-matching cas value will error.
 	Del(key string, cas uint64) error
 
-	// Ephemeral subscriptions to changes to a key.
-	// During a deletion, the CfgEvent.CAS will be 0.
+	// Subscriptions to changes to a key.  During a deletion event,
+	// the CfgEvent.CAS field will be 0.
 	Subscribe(key string, ch chan CfgEvent) error
 }
 
+// The error used on mismatches of CAS (compare and set/swap) values.
 type CfgCASError struct{}
 
-func (e *CfgCASError) Error() string {
-	return "CAS mismatch"
-}
+func (e *CfgCASError) Error() string { return "CAS mismatch" }
 
+// See the Cfg.Subscribe() method.
 type CfgEvent struct {
 	Key string
 	CAS uint64
