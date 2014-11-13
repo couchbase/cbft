0bafd695cd4c0a0f35cb6f24795a26f768656e85
Steve Yen <steve.yen@gmail.com>
2014-11-12 21:12:07 -0800

    split out separate defs.go/defs_test.go

--- manager_test.go
+++ manager_test.go
@@ -12,7 +12,6 @@
 package main
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"os"
 	"reflect"
@@ -383,235 +382,6 @@ func TestManagerRegisterFeed(t *testing.T) {
 	if len(feeds) != 0 || len(pindexes) != 0 {
 		t.Errorf("wrong counts for current feeds (%d) & pindexes (%d)",
 			len(feeds), len(pindexes))
-	}
-}
-
-func TestIndexDefs(t *testing.T) {
-	d := NewIndexDefs("1.2.3")
-	buf, _ := json.Marshal(d)
-	d2 := &IndexDefs{}
-	err := json.Unmarshal(buf, d2)
-	if err != nil || d.UUID != d2.UUID || d.ImplVersion != d2.ImplVersion {
-		t.Errorf("Unmarshal IndexDefs err or mismatch")
-	}
-
-	cfg := NewCfgMem()
-	d3, cas, err := CfgGetIndexDefs(cfg)
-	if err != nil || cas != 0 || d3 != nil {
-		t.Errorf("CfgGetIndexDefs on new cfg should be nil")
-	}
-	cas, err = CfgSetIndexDefs(cfg, d, 100)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetIndexDefs create on new cfg")
-	}
-	cas1, err := CfgSetIndexDefs(cfg, d, 0)
-	if err != nil || cas1 != 1 {
-		t.Errorf("expected ok on first save")
-	}
-	cas, err = CfgSetIndexDefs(cfg, d, 0)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetIndexDefs recreate")
-	}
-	d4, cas, err := CfgGetIndexDefs(cfg)
-	if err != nil || cas != cas1 ||
-		d.UUID != d4.UUID || d.ImplVersion != d4.ImplVersion {
-		t.Errorf("expected get to match first save")
-	}
-}
-
-func TestNodeDefs(t *testing.T) {
-	d := NewNodeDefs("1.2.3")
-	buf, _ := json.Marshal(d)
-	d2 := &NodeDefs{}
-	err := json.Unmarshal(buf, d2)
-	if err != nil || d.UUID != d2.UUID || d.ImplVersion != d2.ImplVersion {
-		t.Errorf("UnmarshalNodeDefs err or mismatch")
-	}
-
-	cfg := NewCfgMem()
-	d3, cas, err := CfgGetNodeDefs(cfg, NODE_DEFS_KNOWN)
-	if err != nil || cas != 0 || d3 != nil {
-		t.Errorf("CfgGetNodeDefs on new cfg should be nil")
-	}
-	cas, err = CfgSetNodeDefs(cfg, NODE_DEFS_KNOWN, d, 100)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetNodeDefs create on new cfg")
-	}
-	cas1, err := CfgSetNodeDefs(cfg, NODE_DEFS_KNOWN, d, 0)
-	if err != nil || cas1 != 1 {
-		t.Errorf("expected ok on first save")
-	}
-	cas, err = CfgSetNodeDefs(cfg, NODE_DEFS_KNOWN, d, 0)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetNodeDefs recreate")
-	}
-	d4, cas, err := CfgGetNodeDefs(cfg, NODE_DEFS_KNOWN)
-	if err != nil || cas != cas1 ||
-		d.UUID != d4.UUID || d.ImplVersion != d4.ImplVersion {
-		t.Errorf("expected get to match first save")
-	}
-}
-
-func TestPlanPIndexes(t *testing.T) {
-	d := NewPlanPIndexes("1.2.3")
-	buf, _ := json.Marshal(d)
-	d2 := &PlanPIndexes{}
-	err := json.Unmarshal(buf, d2)
-	if err != nil || d.UUID != d2.UUID || d.ImplVersion != d2.ImplVersion {
-		t.Errorf("UnmarshalPlanPIndexes err or mismatch")
-	}
-
-	cfg := NewCfgMem()
-	d3, cas, err := CfgGetPlanPIndexes(cfg)
-	if err != nil || cas != 0 || d3 != nil {
-		t.Errorf("CfgGetPlanPIndexes on new cfg should be nil")
-	}
-	cas, err = CfgSetPlanPIndexes(cfg, d, 100)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetPlanPIndexes create on new cfg")
-	}
-	cas1, err := CfgSetPlanPIndexes(cfg, d, 0)
-	if err != nil || cas1 != 1 {
-		t.Errorf("expected ok on first save")
-	}
-	cas, err = CfgSetPlanPIndexes(cfg, d, 0)
-	if err == nil || cas != 0 {
-		t.Errorf("expected error on CfgSetPlanPIndexes recreate")
-	}
-	d4, cas, err := CfgGetPlanPIndexes(cfg)
-	if err != nil || cas != cas1 ||
-		d.UUID != d4.UUID || d.ImplVersion != d4.ImplVersion {
-		t.Errorf("expected get to match first save")
-	}
-}
-
-func TestSamePlanPIndexes(t *testing.T) {
-	a := NewPlanPIndexes("0.0.1")
-	b := NewPlanPIndexes("0.0.1")
-	c := NewPlanPIndexes("0.1.0")
-
-	if !SamePlanPIndexes(a, b) {
-		t.Errorf("expected same, a: %v, b: %v", a, b)
-	}
-	if !SamePlanPIndexes(a, b) {
-		t.Errorf("expected same, a: %v, b: %v", a, b)
-	}
-	if !SamePlanPIndexes(a, c) {
-		t.Errorf("expected same, a: %v, c: %v", a, c)
-	}
-	if !SamePlanPIndexes(c, a) {
-		t.Errorf("expected same, a: %v, c: %v", a, c)
-	}
-
-	a.PlanPIndexes["foo"] = &PlanPIndex{
-		Name: "foo",
-	}
-
-	if SamePlanPIndexes(a, b) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-	if SamePlanPIndexes(b, a) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-	if SamePlanPIndexes(a, c) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-	if SamePlanPIndexes(c, a) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-
-	if SubsetPlanPIndexes(a, b) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-	if !SubsetPlanPIndexes(b, a) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-
-	b.PlanPIndexes["foo"] = &PlanPIndex{
-		Name:      "foo",
-		IndexName: "differnet-than-foo-in-a",
-	}
-
-	if SamePlanPIndexes(a, b) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-	if SamePlanPIndexes(b, a) {
-		t.Errorf("expected not same, a: %v, b: %v", a, b)
-	}
-}
-
-func TestSamePlanPIndex(t *testing.T) {
-	ppi0 := &PlanPIndex{
-		Name:             "0",
-		UUID:             "x",
-		IndexName:        "x",
-		IndexUUID:        "x",
-		IndexSchema:      "x",
-		SourceType:       "x",
-		SourceName:       "x",
-		SourceUUID:       "x",
-		SourcePartitions: "x",
-		NodeUUIDs:        make(map[string]string),
-	}
-	ppi1 := &PlanPIndex{
-		Name:             "1",
-		UUID:             "x",
-		IndexName:        "x",
-		IndexUUID:        "x",
-		IndexSchema:      "x",
-		SourceType:       "x",
-		SourceName:       "x",
-		SourceUUID:       "x",
-		SourcePartitions: "x",
-		NodeUUIDs:        make(map[string]string),
-	}
-
-	if !SamePlanPIndex(ppi0, ppi0) {
-		t.Errorf("expected SamePlanPindex to be true")
-	}
-	if SamePlanPIndex(ppi0, ppi1) {
-		t.Errorf("expected SamePlanPindex to be false")
-	}
-	if SamePlanPIndex(ppi1, ppi0) {
-		t.Errorf("expected SamePlanPindex to be false")
-	}
-}
-
-func TestPIndexMatchesPlan(t *testing.T) {
-	plan := &PlanPIndex{
-		Name: "hi",
-		UUID: "111",
-	}
-	px := &PIndex{
-		Name: "hi",
-		UUID: "222",
-	}
-	py := &PIndex{
-		Name: "hello",
-		UUID: "111",
-	}
-	if PIndexMatchesPlan(px, plan) == false {
-		t.Errorf("expected pindex to match the plan")
-	}
-	if PIndexMatchesPlan(py, plan) == true {
-		t.Errorf("expected pindex to not match the plan")
-	}
-}
-
-func TestCfgGetHelpers(t *testing.T) {
-	errCfg := &ErrorOnlyCfg{}
-
-	if _, err := CheckVersion(errCfg, "my-version"); err == nil {
-		t.Errorf("expected to fail with errCfg")
-	}
-	if _, _, err := CfgGetIndexDefs(errCfg); err == nil {
-		t.Errorf("expected to fail with errCfg")
-	}
-	if _, _, err := CfgGetNodeDefs(errCfg, NODE_DEFS_KNOWN); err == nil {
-		t.Errorf("expected to fail with errCfg")
-	}
-	if _, _, err := CfgGetPlanPIndexes(errCfg); err == nil {
-		t.Errorf("expected to fail with errCfg")
 	}
 }
 
@@ -1194,6 +964,7 @@ func testPartitioning(t *testing.T,
 		t.Errorf("expected CreateIndex() to work")
 	}
 
+	mgr.Kick("test")
 	mgr.PlannerNOOP("test")
 	mgr.JanitorNOOP("test")
 	feeds, pindexes := mgr.CurrentMaps()
@@ -1400,6 +1171,7 @@ func TestFanInPartitioningMutations(t *testing.T) {
 				t.Errorf("expected no error to rollback, err: %v", err)
 			}
 			runtime.Gosched()
+			mgr.Kick("after-rollback")
 			mgr.PlannerNOOP("after-rollback")
 			mgr.JanitorNOOP("after-rollback")
 			runtime.Gosched()
