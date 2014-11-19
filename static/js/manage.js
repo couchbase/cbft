ea82f12c3c4a824c8cd999b8835b3b577081fdfe
Steve Yen <steve.yen@gmail.com>
2014-11-19 10:07:35 -0800

    web UI - listing nodes on Manage tab

--- static/js/manage.js
+++ static/js/manage.js
@@ -1,28 +1,32 @@
 function ManageCtrl($scope, $http, $routeParams, $log, $sce, $location) {
 
-	$scope.resultMessage = null;
     $scope.resultCfg = null;
+    $scope.resultCfgJSON = null;
+    $scope.resultMessage = null;
 
-	$scope.managerKick = function(managerKickMsg) {
-		$scope.resultMessage = null;
-		$http.post('/api/managerKick?msg=' + managerKickMsg).
-		success(function(data) {
+    $scope.managerKick = function(managerKickMsg) {
+        $scope.resultMessage = null;
+        $http.post('/api/managerKick?msg=' + managerKickMsg).
+        success(function(data) {
             $scope.resultMessage = data.status;
-		}).
-		error(function(data, code) {
-			$scope.resultMessage = data.status;
-		});
-	};
+        }).
+        error(function(data, code) {
+            $scope.resultMessage = data.status;
+        });
+    };
 
-	$scope.refreshCfg = function(name, mapping) {
+    $scope.refreshCfg = function(name, mapping) {
         $scope.resultCfg = null;
-		$http.get('/api/cfg').success(function(data) {
-            $scope.resultCfg = JSON.stringify(data, undefined, 2);
-		}).
-		error(function(data, code) {
+        $scope.resultCfgJSON = null;
+        $http.get('/api/cfg').success(function(data) {
             $scope.resultCfg = data;
-		});
-	};
+            $scope.resultCfgJSON = JSON.stringify(data, undefined, 2);
+        }).
+        error(function(data, code) {
+            $scope.resultCfg = data;
+            $scope.resultCfgJSON = JSON.stringify(data, undefined, 2);
+        });
+    };
 
     $scope.refreshCfg();
 }
