b7f4f82bc13741b45deadf7dc88674def3ad2897
Steve Yen <steve.yen@gmail.com>
2014-11-24 20:26:20 -0800

    started user defined alias implementation

--- pindex_impl_alias.go
+++ pindex_impl_alias.go
@@ -11,8 +11,109 @@
 
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/blevesearch/bleve"
+)
+
 func init() {
 	// Register alias with empty instantiation functions,
 	// so that "alias" will show up in valid index types.
-	RegisterPIndexImplType("alias", &PIndexImplType{})
+	RegisterPIndexImplType("alias", &PIndexImplType{
+		Count: CountAlias,
+		Query: QueryAlias,
+	})
+}
+
+// AliasSourceSchema holds the source definition for a user-defined
+// index alias.  An index alias can be used as a level of indirection
+// (the "LastQuartersSales" alias points currently to the
+// "2014-Q3-Sales" index, but the administrator might repoint it in
+// the future without changing the application) or to scatter-gather
+// or fan-out a query across multiple real indexes (e.g., to query
+// across customer records, product catalog, call-center records, etc,
+// in one shot).
+type AliasSourceSchema struct {
+	Sources map[string]*AliasSource `json:"sources"` // Keyed by indexName.
+}
+
+type AliasSource struct {
+	IndexUUID string `json:"indexUUID"` // Optional.
+}
+
+func CountAlias(mgr *Manager, indexName, indexUUID string) (uint64, error) {
+	alias, err := bleveIndexAliasForUserIndexAlias(mgr, indexName, indexUUID)
+	if err != nil {
+		return 0, fmt.Errorf("CountAlias indexAlias error,"+
+			" indexName: %s, indexUUID: %s, err: %v", indexName, indexUUID, err)
+	}
+
+	return alias.DocCount()
+}
+
+func QueryAlias(mgr *Manager, indexName, indexUUID string,
+	req []byte, res io.Writer) error {
+	alias, err := bleveIndexAliasForUserIndexAlias(mgr, indexName, indexUUID)
+	if err != nil {
+		return fmt.Errorf("QueryAlias indexAlias error,"+
+			" indexName: %s, indexUUID: %s, err: %v", indexName, indexUUID, err)
+	}
+
+	var searchRequest bleve.SearchRequest
+	err = json.Unmarshal(req, &searchRequest)
+	if err != nil {
+		return fmt.Errorf("QueryBlevePIndexImpl parsing req, err: %v", err)
+	}
+
+	err = searchRequest.Query.Validate()
+	if err != nil {
+		return err
+	}
+
+	searchResponse, err := alias.Search(&searchRequest)
+	if err != nil {
+		return err
+	}
+
+	mustEncode(res, searchResponse)
+
+	return nil
+}
+
+// The indexName/indexUUID is for a user-defined index alias.
+func bleveIndexAliasForUserIndexAlias(mgr *Manager,
+	indexName, indexUUID string) (bleve.IndexAlias, error) {
+	// TODO: One day should support user-defined aliases for non-bleve indexes.
+	alias := bleve.NewIndexAlias()
+
+	indexDefs, _, err := CfgGetIndexDefs(mgr.cfg)
+	if err != nil {
+		return nil, fmt.Errorf("could not get indexDefs, indexName: %s, err: %v",
+			indexName, err)
+	}
+	indexDef := indexDefs.IndexDefs[indexName]
+	if indexDef == nil {
+		return nil, fmt.Errorf("could not get indexDef, indexName: %s", indexName)
+	}
+
+	schema := AliasSourceSchema{}
+	err = json.Unmarshal([]byte(indexDef.Schema), &schema)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse indexDef.Schema: %s, indexName: %s",
+			indexDef.Schema, indexName)
+	}
+
+	for indexName, source := range schema.Sources {
+		subAlias, err := bleveIndexAlias(mgr, indexName, source.IndexUUID)
+		if err != nil {
+			return nil, fmt.Errorf("could not get subAlias, indexName: %s,"+
+				" source: %#v, err: %v", indexName, source, err)
+		}
+		alias.Add(subAlias)
+	}
+
+	return alias, nil
 }
