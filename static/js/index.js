6a50fcab8ddc00b5aa1eeaaa2c90051be6781b92
Steve Yen <steve.yen@gmail.com>
2014-12-01 23:02:53 -0800

    show index partition layout by vbucket & node in a table

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,14 @@
 function IndexesCtrl($scope, $http, $routeParams, $log, $sce) {
 
-	$scope.indexNames = [];
-	$scope.errorMessage = null;
+    $scope.indexNames = [];
+    $scope.errorMessage = null;
 
-	$scope.clearErrorMessage = function() {
-		$scope.errorMessage = null;
-	};
+    $scope.clearErrorMessage = function() {
+        $scope.errorMessage = null;
+    };
 
-	$scope.refreshIndexNames = function() {
-		$http.get('/api/index').success(function(data) {
+    $scope.refreshIndexNames = function() {
+        $http.get('/api/index').success(function(data) {
             var indexNames = [];
             for (var indexName in data.indexDefs.indexDefs) {
                 indexNames.push(indexName)
@@ -16,90 +16,106 @@ function IndexesCtrl($scope, $http, $routeParams, $log, $sce) {
             $scope.indexNames = indexNames;
         }).
         error(function(data, code) {
-			$scope.errorMessage = data;
+            $scope.errorMessage = data;
         });
-	};
+    };
 
-	$scope.deleteIndexNamed = function(name) {
-		if(!confirm("Are you sure you want to permanenty delete the index '" + name + "'?")) {
-			return;
-		}
-		$scope.clearErrorMessage();
-		$http.delete('/api/index/' + name).success(function(data) {
-			$scope.refreshIndexNames();
-		}).
-		error(function(data, code) {
-			$scope.errorMessage = data;
-		});
-	};
+    $scope.deleteIndexNamed = function(name) {
+        if(!confirm("Are you sure you want to permanenty delete the index '"
+                    + name + "'?")) {
+            return;
+        }
+        $scope.clearErrorMessage();
+        $http.delete('/api/index/' + name).success(function(data) {
+            $scope.refreshIndexNames();
+        }).
+        error(function(data, code) {
+            $scope.errorMessage = data;
+        });
+    };
 
-	$scope.refreshIndexNames();
+    $scope.refreshIndexNames();
 }
 
 function IndexCtrl($scope, $http, $routeParams, $log, $sce) {
 
-	$scope.indexName = $routeParams.indexName;
-	$scope.indexDocCount = 0;
-	$scope.indexDefStr = "";
-	$scope.indexParamsStr = "";
-	$scope.tab = $routeParams.tabName;
-	if($scope.tab === undefined || $scope.tab === "") {
-		$scope.tab = "summary";
-	}
-	$scope.tabPath = '/static/partials/index/tab-' + $scope.tab + '.html';
+    $scope.nodeDefsByUUID = null;
 
-	$scope.loadIndexDetails = function() {
-		$http.get('/api/index/' + $scope.indexName).success(function(data) {
+    $scope.indexName = $routeParams.indexName;
+    $scope.indexDocCount = 0;
+    $scope.indexDefStr = "";
+    $scope.indexParamsStr = "";
+    $scope.planPIndexes = null;
+    $scope.planPIndexesStr = ""
+    $scope.tab = $routeParams.tabName;
+    if($scope.tab === undefined || $scope.tab === "") {
+        $scope.tab = "summary";
+    }
+    $scope.tabPath = '/static/partials/index/tab-' + $scope.tab + '.html';
+
+    $http.get('/api/cfg').success(function(data) {
+        $scope.nodeDefsByUUID = {}
+        for (var k in data.nodeDefsKnown.nodeDefs) {
+            var nodeDef = data.nodeDefsKnown.nodeDefs[k]
+            $scope.nodeDefsByUUID[nodeDef.uuid] = nodeDef
+        }
+        $scope.loadIndexDetails()
+    })
+
+    $scope.loadIndexDetails = function() {
+        $http.get('/api/index/' + $scope.indexName).success(function(data) {
             data.indexDef.params = JSON.parse(data.indexDef.params)
             data.indexDef.sourceParams = JSON.parse(data.indexDef.sourceParams)
             $scope.indexDefStr = JSON.stringify(data.indexDef, undefined, 2)
             $scope.indexParamsStr = JSON.stringify(data.indexDef.params, undefined, 2)
-            $scope.indexLayoutStr = JSON.stringify(data.planPIndexes, undefined, 2)
+            $scope.planPIndexesStr = JSON.stringify(data.planPIndexes, undefined, 2)
+            $scope.planPIndexes = data.planPIndexes
         }).
         error(function(data, code) {
-			$scope.errorMessage = data;
+            $scope.errorMessage = data;
         });
-	};
+    };
 
-	$scope.loadIndexDocCount = function() {
-		$http.get('/api/index/' + $scope.indexName + '/count').success(function(data) {
+    $scope.loadIndexDocCount = function() {
+        $http.get('/api/index/' + $scope.indexName + '/count').
+        success(function(data) {
             $scope.indexDocCount = data.count;
         }).
         error(function(data, code) {
-			$scope.errorMessage = data;
+            $scope.errorMessage = data;
         });
-	};
+    };
 
-	// always load the details
-	$scope.loadIndexDetails();
-	// tab specific loading
-	if($scope.tab === "summary") {
-		$scope.loadIndexDocCount();
-	}
+    // tab specific loading
+    if($scope.tab === "summary") {
+        $scope.loadIndexDocCount();
+    }
 
-	$scope.indexDocument = function(id, body) {
-		$scope.clearErrorMessage();
-		$http.put('/api/index/' + $scope.indexName + "/doc/" + id, body).success(function(data) {
-			$scope.successMessage = "Indexed Document: " + id;
-		}).
-		error(function(data, code) {
-			$scope.errorMessage = data;
-		});
-	};
+    $scope.indexDocument = function(id, body) {
+        $scope.clearErrorMessage();
+        $http.put('/api/index/' + $scope.indexName + "/doc/" + id, body).
+        success(function(data) {
+            $scope.successMessage = "Indexed Document: " + id;
+        }).
+        error(function(data, code) {
+            $scope.errorMessage = data;
+        });
+    };
 
-	$scope.deleteDocument = function(id) {
-		$scope.clearErrorMessage();
-		$http.delete('/api/index/' + $scope.indexName + "/" + id).success(function(data) {
-			$scope.successMessage = "Deleted Document: " + id;
-		}).
-		error(function(data, code) {
-			$scope.errorMessage = data;
-		});
-	};
+    $scope.deleteDocument = function(id) {
+        $scope.clearErrorMessage();
+        $http.delete('/api/index/' + $scope.indexName + "/" + id).
+        success(function(data) {
+            $scope.successMessage = "Deleted Document: " + id;
+        }).
+        error(function(data, code) {
+            $scope.errorMessage = data;
+        });
+    };
 
     $scope.clearErrorMessage = function() {
-		$scope.errorMessage = null;
-	};
+        $scope.errorMessage = null;
+    };
 }
 
 function IndexNewCtrl($scope, $http, $routeParams, $log, $sce, $location) {
@@ -108,8 +124,8 @@ function IndexNewCtrl($scope, $http, $routeParams, $log, $sce, $location) {
     $scope.newPlanParams = "";
     $scope.paramNumLines = {};
 
-	$http.get('/api/managerMeta').success(function(data) {
-		$scope.meta = data;
+    $http.get('/api/managerMeta').success(function(data) {
+        $scope.meta = data;
 
         for (var k in data.sourceTypes) {
             $scope.newSourceParams[k] =
@@ -123,37 +139,40 @@ function IndexNewCtrl($scope, $http, $routeParams, $log, $sce, $location) {
             $scope.paramNumLines[k] = $scope.newIndexParams[k].split("\n").length;
         }
 
-        $scope.newPlanParams = JSON.stringify(data.startSamples["planParams"], undefined, 2);
-        $scope.paramNumLines["planParams"] = $scope.newPlanParams.split("\n").length;
-	})
+        $scope.newPlanParams =
+            JSON.stringify(data.startSamples["planParams"], undefined, 2);
+        $scope.paramNumLines["planParams"] =
+            $scope.newPlanParams.split("\n").length;
+    })
 
-	$scope.errorMessage = null;
+    $scope.errorMessage = null;
 
-	$scope.newIndexNamed = function(sourceType, sourceName, sourceUUID, sourceParams,
-									indexType, indexName, indexParams,
-									planParams) {
-		$scope.clearErrorMessage();
-		$http.put('/api/index/' + indexName, "", {
-			params: {
-			    sourceType: sourceType,
-			    sourceName: sourceName,
-			    sourceUUID: sourceUUID || "",
-			    sourceParams: sourceParams[sourceType],
-			    indexType: indexType || "bleve",
-			    indexName: indexName,
+    $scope.newIndexNamed = function(sourceType, sourceName,
+                                    sourceUUID, sourceParams,
+                                    indexType, indexName, indexParams,
+                                    planParams) {
+        $scope.clearErrorMessage();
+        $http.put('/api/index/' + indexName, "", {
+            params: {
+                sourceType: sourceType,
+                sourceName: sourceName,
+                sourceUUID: sourceUUID || "",
+                sourceParams: sourceParams[sourceType],
+                indexType: indexType || "bleve",
+                indexName: indexName,
                 indexParams: indexParams[indexType],
-			    planParams: planParams,
-		    }
-		}).
-		success(function(data) {
-			$location.path('/indexes/' + indexName);
-		}).
-		error(function(data, code) {
-			$scope.errorMessage = data;
-		});
-	};
+                planParams: planParams,
+            }
+        }).
+        success(function(data) {
+            $location.path('/indexes/' + indexName);
+        }).
+        error(function(data, code) {
+            $scope.errorMessage = data;
+        });
+    };
 
-	$scope.clearErrorMessage = function() {
-		$scope.errorMessage = null;
-	};
+    $scope.clearErrorMessage = function() {
+        $scope.errorMessage = null;
+    };
 }
