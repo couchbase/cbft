0a3af2beae07a1a9aa4b6c7d1e373270310da2cf
Steve Yen <steve.yen@gmail.com>
2014-12-04 14:41:10 -0800

    generate a color for each rack/row/zone/region component

--- static/js/node.js
+++ static/js/node.js
@@ -10,10 +10,25 @@ function NodeCtrl($scope, $http, $routeParams, $log, $sce, $location) {
     }
     $scope.tabPath = '/static/partials/node/tab-' + $scope.tab + '.html';
 
+    $scope.containerPartColor = function(s) {
+        var r = 3.14159;
+        for (var i = 0; i < s.length; i++) {
+            r = r ^ (r * s.charCodeAt(i));
+        }
+        v = Math.abs(r).toString(16);
+        v1 = v.slice(0, 1);
+        v2 = v.slice(1, 3);
+        return ('#1' + v1 + v2 + v2);
+    }
+
     $scope.cfgGet = function(name, mapping) {
         $scope.resultCfg = null;
         $scope.resultCfgJSON = null;
         $http.get('/api/cfg').success(function(data) {
+            for (var nodeAddr in data.nodeDefsKnown.nodeDefs) {
+                var nodeDef = data.nodeDefsKnown.nodeDefs[nodeAddr];
+                nodeDef.containerArr = (nodeDef.container || "").split('/');
+            }
             $scope.resultCfg = data;
             $scope.resultCfgJSON = JSON.stringify(data, undefined, 2);
         }).
