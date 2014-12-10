62292a1b55bf2b62c1f217bcf52e60fc29019f1f
Steve Yen <steve.yen@gmail.com>
2014-12-10 14:01:43 -0800

    TestNewPIndexBleveMem()

--- pindex_test.go
+++ pindex_test.go
@@ -147,3 +147,24 @@ func TestBlackholePIndexImpl(t *testing.T) {
 		t.Errorf("expected blackhole query to err")
 	}
 }
+
+func TestNewPIndexBleveMem(t *testing.T) {
+	emptyDir, _ := ioutil.TempDir("./tmp", "test")
+	defer os.RemoveAll(emptyDir)
+
+	pindex, err := NewPIndex(nil, "fake", "uuid",
+		"bleve-mem", "indexName", "indexUUID", "",
+		"sourceType", "sourceName", "sourceUUID", "sourceParams", "sourcePartitions",
+		PIndexPath(emptyDir, "fake"))
+	if pindex == nil || err != nil {
+		t.Errorf("expected NewPIndex to work")
+	}
+	pindex2, err := OpenPIndex(nil, PIndexPath(emptyDir, "fake"))
+	if pindex2 != nil || err == nil {
+		t.Errorf("expected OpenPIndex to fail for bleve-mem")
+	}
+	err = pindex.Close(true)
+	if err != nil {
+		t.Errorf("expected Close to work")
+	}
+}
