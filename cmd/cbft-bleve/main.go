4e3a7b9e9a3766fea1dc59c7e0288605ab436316
Steve Yen <steve.yen@gmail.com>
2016-10-19 10:32:45 -0700

    MB-21312 - refresh FTS cmd-line tools
    The various previously copy-&-pasted cmd-line tools are now replaced
    in this commit by a single cbft-bleve tool, that's powered by bleve's
    cmd feature.
    
    Change-Id: I9ad9509a4363d4f1b5f320927bdad03bfdf06f31
    Reviewed-on: http://review.couchbase.org/68943
    Reviewed-by: Marty Schoch <marty.schoch@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

---
+++ cmd/cbft-bleve/main.go
@@ -0,0 +1,32 @@
+// Copyright © 2016 Couchbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/blevesearch/bleve/cmd/bleve/cmd"
+
+	bleveMoss "github.com/blevesearch/bleve/index/store/moss"
+
+	// to support cbft's flavor of bleve build tags & flags
+	_ "github.com/couchbase/cbft"
+
+	"github.com/couchbase/moss"
+)
+
+func main() {
+	bleveMoss.RegistryCollectionOptions["fts"] = moss.CollectionOptions{}
+
+	cmd.Execute()
+}
