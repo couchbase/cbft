couchbase/cbft

commit e5f0311172386189c82d96d5163796ef72b9e68b

MB-21862 - spock-UI: allow empty string for type mappings
Previously, the UI javascript used a tracking map that treated the
empty string ("") key as the default mapping, which incorrectly
prevented users from using the empty string as a type name in the UI.

This change generates a random, synthetic string key to represent the
default mapping in the tracking map.

Change-Id: I0fdd86dd669c1ee6556f01032c4892f3536b64b0
Reviewed-on: http://review.couchbase.org/73983
Reviewed-by: Marty Schoch <marty.schoch@gmail.com>
Tested-by: Steve Yen <steve.yen@gmail.com>

ns_server_static/fts/static-bleve-mapping/js/mapping/index-mapping.js
@@ -7,6 +7,9 @@ function initBleveIndexMappingController(
 	var indexMapping =
         $scope.indexMapping = JSON.parse(JSON.stringify(indexMappingIn));
 
+    indexMapping.defaultMappingKey = "defaultMappingKey_" +
+        Math.random() + Math.random() + Math.random();
+
     indexMapping.types =
         indexMapping.types || {};
     indexMapping.analysis =
@@ -23,10 +26,11 @@ function initBleveIndexMappingController(
         indexMapping.analysis.token_maps || {};
 
     if (indexMapping["default_mapping"]) {
-        indexMapping.types[""] = indexMapping["default_mapping"];
+        indexMapping.types[indexMapping.defaultMappingKey] = indexMapping["default_mapping"];
     }
 
-    var tmc = initBleveTypeMappingController($scope, indexMapping.types, options);
+    var tmc = initBleveTypeMappingController($scope, indexMapping.types,
+                                             indexMapping.defaultMappingKey, options);
 
     $scope.isValid = function() { return tmc.isValid(); };
 
@@ -86,8 +90,9 @@ function initBleveIndexMappingController(
         var r = JSON.parse(JSON.stringify($scope.indexMapping));
 
         r.types = tmc.typeMapping();
-        r.default_mapping = r.types[""];
-        delete r.types[""];
+        r.default_mapping = r.types[indexMapping.defaultMappingKey];
+        delete r.types[indexMapping.defaultMappingKey];
+        delete r["defaultMappingKey"]
 
         return JSON.parse(JSON.stringify(scrub(r)));
     }

ns_server_static/fts/static-bleve-mapping/js/mapping/type-mapping.js
@@ -206,8 +206,7 @@ function initBleveTypeMappingController($scope, typeMappingIn, options) {
 }
 
 // Convert from a near-bleve-friendly TypeMapping data structure to a
-// UI-friendly data structure.  By "near", an entry with "" key
-// represents the default type mapping.
+// UI-friendly data structure.
 function bleveConvertFromTypeMapping(typeMapping) {
     var mappings = [];
 
@@ -277,9 +276,8 @@ function bleveConvertFromTypeMapping(typeMapping) {
     }
 }
 
-// Convert froma UI-friendly data structure to a near-bleve-friendly
-// TypeMapping data structure.  By "near", an entry with "" key
-// represents the default type mapping.
+// Convert from a UI-friendly data structure to a near-bleve-friendly
+// TypeMapping data structure.
 function bleveConvertToTypeMapping(mappings) {
     var typeMapping = {};
 

ns_server_static/fts/static-bleve-mapping/partials/mapping/type-mapping-tree.html
@@ -13,12 +13,12 @@
       {}
     </span>
 
-    <span ng-if="mapping.name == null">
+    <span ng-if="mapping.name == indexMapping.defaultMappingKey">
       <i>default</i>
     </span>
 
     <span ng-if="!mapping._editing">
-      <span ng-if="mapping.name">
+      <span ng-if="mapping.name != indexMapping.defaultMappingKey">
         {{mapping.name}}
       </span>
 
@@ -30,7 +30,7 @@
           | only index specified fields</span>
       </span>
     </span>
-    <span ng-if="mapping._editing && mapping.name != null"
+    <span ng-if="mapping._editing && mapping.name != indexMapping.defaultMappingKey"
           data-nodrag>
       <input ng-if="mapping._kind == 'mappingType'"
              type="text" placeholder="please enter a type name"
@@ -103,7 +103,7 @@
          data-nodrag
          ng-click="editAttrsDone(mapping, false)">
          <i class="glyphicon glyphicon-remove"></i></a>
-      <a ng-if="mapping._editing === true && mapping.name != null"
+      <a ng-if="mapping._editing === true && mapping.name != indexMapping.defaultMappingKey"
          class="btn btn-xs btn-trash"
          data-nodrag
          ng-click="removeFromParent(mapping, this)">

commit d1d0c54e4019b59919b33b75b7bc09dcb3f72f76

MB-23668 - UI: field store checkbox defaults to false
Regression introduced in commit e5f03111, while trying to fix a bug in
handling empty string names for type mappings, I incorrectly changed a
function call signature.

Change-Id: Ibba0d403f8657eddfd1ec98e361057a081fd9028
Reviewed-on: http://review.couchbase.org/76221
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Marty Schoch <marty.schoch@gmail.com>

ns_server_static/fts/static-bleve-mapping/js/mapping/index-mapping.js
@@ -29,8 +29,7 @@ function initBleveIndexMappingController(
         indexMapping.types[indexMapping.defaultMappingKey] = indexMapping["default_mapping"];
     }
 
-    var tmc = initBleveTypeMappingController($scope, indexMapping.types,
-                                             indexMapping.defaultMappingKey, options);
+    var tmc = initBleveTypeMappingController($scope, indexMapping.types, options);
 
     $scope.isValid = function() { return tmc.isValid(); };
 