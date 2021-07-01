992167ff3ed78b0e56a17c0b83f5e098e3107526
Blair Watt <blair.watt@couchbase.com>
2021-07-01 11:29:11 +0100

    CBD-4161: Convert remaining Server repositories to BSL
    Change-Id: I3e17c68616dc1582c62fd9b8151b176316aef343
    Reviewed-on: http://review.couchbase.org/c/cbft/+/156755
    Reviewed-by: Abhinav Dangeti <abhinav@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- ns_server_static/fts/main.js
+++ ns_server_static/fts/main.js
@@ -1,3 +1,13 @@
+/*
+Copyright 2020-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 import angular from "/ui/web_modules/angular.js";
 import app from "/ui/app/app.js";
 import mnElementCrane from "/ui/app/components/directives/mn_element_crane/mn_element_crane.js";
