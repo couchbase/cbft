e0f3e9980806c1bc5eb645f104ccdd3b35e5e29e
Abhinav Dangeti <abhinav@couchbase.com>
2024-05-16 10:06:18 -0600

    MB-59575, MB-60943: Bump up bleve/v2, zapx/v16, go-faiss, faiss
    Pre-req from blevesearch/faiss (to be updated with 1.7.4-6 cbdeps build):
    * https://review.couchbase.org/c/tlm/+/210090
        * 7c3c7d1 Thejas-bhat | MB-59575: Refactor member variables alignment of IndexFlatCodes (#22)
        * 17c3992 Thejas-bhat | MB-59575: Reducing copy overhead of already memory mapped content (#17)
    
    Brings in:
    * 11c84865 Abhi Dangeti | MB-60943: Upgrade to zapx/v16@v16.1.0 (#2036)
        * cdfd338 Aditi Ahuja | MB-60943 - Change quantiser when vector index is tuned for memory (#244)
        * f4827a8 Aditi Ahuja | MB-60943 - Add option for memory-optimised vector indexes. (#49)
    * 9f227fe2 Abhi Dangeti | MB-59575: Upgrade zapx/v16, go-faiss (#2034)
        * 8c1937d Thejas-bhat | bug fix: de-duplicating fieldsInv value (#239)
        * 08059f8 Thejas-bhat | MB-59575: Handling memory mapped content more responsibly  (#241)
            * 5e22b9d Thejas-bhat | MB-59575: Handling memory mapped content more responsibly (#26)
    
    + Also fixes a formatting issue in JSON emitted for /api/log for certain
      system events.
    
    Change-Id: I51c962cc07cf700d787ef5a54152e9887201b15d
    Reviewed-on: https://review.couchbase.org/c/cbft/+/210085
    Tested-by: Abhi Dangeti <abhinav@couchbase.com>
    Reviewed-by: Abhi Dangeti <abhinav@couchbase.com>
    Well-Formed: Restriction Checker
    Well-Formed: Build Bot <build@couchbase.com>

--- pindex_bleve.go
+++ pindex_bleve.go
@@ -2024,7 +2024,8 @@ func (t *BleveDest) Stats(w io.Writer) (err error) {
 
 	// exit early if all details are disabled.
 	if !verbose && !vbstats {
-		return
+		_, err = w.Write([]byte(`{}`))
+		return err
 	}
 
 	// if verbose stats is requested then send most of the index stats.
