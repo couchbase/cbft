couchbase/cbft

commit ab60e06b824fdde036a7fbf38994d79027a7d2e4

MB-18262: Fix UI in search results page
- Add a space after comma in search results page for stored fields containing arrays.
- In https://github.com/blevesearch/bleve/pull/1465, if the highlighter is set, then the highlighted field's value has escaped HTML characters. But we always escape the HTML characters for all the fields' values regardless leading to double escaping the `&` character leading to garbage to be shown in the UI. Added a fix that skips escaping HTML characters for highlighted fields.

Change-Id: Iff1bac0f6a91ccdaea056da411a69f08c10ad297
Reviewed-on: https://review.couchbase.org/c/cbft/+/213997
Well-Formed: Restriction Checker
Reviewed-by: Abhi Dangeti <abhinav@couchbase.com>
Tested-by: Rahul Rampure <rahul.rampure@couchbase.com>
Well-Formed: Build Bot <build@couchbase.com>

ns_server_static/fts/static/query.js
@@ -261,10 +261,14 @@ function QueryCtrl($scope, $http, $routeParams, $log, $sce, $location, qwDialogS
     };
 
     $scope.manualEscapeHtmlExceptHighlighting = function(orig) {
+        if (/<mark>.*<\/mark>/.test(orig)) {
+            // If <mark> ...</mark> tags are present, return the original input as-is
+            // as the string already contains escaped HTML characters
+            return orig;
+        }
         // escape HTML tags
         let updated = orig.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;")
         // find escaped <mark> and </mark> and put them back
-        updated = updated.replace(/&lt;mark&gt;/g, "<mark>").replace(/&lt;\/mark&gt;/g, "</mark>")
         return updated
     }
 
@@ -401,7 +405,14 @@ function QueryCtrl($scope, $http, $routeParams, $log, $sce, $location, qwDialogS
             for(var fv in hit.fields) {
                 var fieldval = hit.fields[fv];
                 if (hit.fragments[fv] === undefined) {
-                    hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(""+fieldval)];
+                    // Check if fieldval is an array
+                    if (Array.isArray(fieldval)) {
+                        // Join array elements with a comma and space, and then escape HTML
+                        hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(fieldval.join(', '))];
+                    } else {
+                        // Handle single string case
+                        hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(fieldval)];
+                    }
                 }
             }
             if ($scope.decorateSearchHit) {

commit 961433a280f7713b102865922a78c9540626ea32

MB-63346: Fix fragment highlighting that regressed with ab60e06b
+ This causes document linking to fail and even the returned stored
  fields to be incomplete.
+ Culprit commit:
    * ab60e06b CascadingRadium | MB-18262: Fix UI in search results page

Change-Id: I7f2e94fb3afc02ac711e2d51575d2275a340e70b
Reviewed-on: https://review.couchbase.org/c/cbft/+/215589
Well-Formed: Restriction Checker
Tested-by: Abhi Dangeti <abhinav@couchbase.com>
Reviewed-by: Abhi Dangeti <abhinav@couchbase.com>

ns_server_static/fts/static/query.js
@@ -411,7 +411,7 @@ function QueryCtrl($scope, $http, $routeParams, $log, $sce, $location, qwDialogS
                         hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(fieldval.join(', '))];
                     } else {
                         // Handle single string case
-                        hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(fieldval)];
+                        hit.fragments[fv] = [$scope.manualEscapeHtmlExceptHighlighting(""+fieldval)];
                     }
                 }
             }